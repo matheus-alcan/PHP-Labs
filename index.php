0f60c3866659f31257e1122f3c25c277e1f6bd27
Matheus Alcântara <167790351+matheus-alcan@users.noreply.github.com>
2025-10-08 19:51:58 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\styles.css">
+    <title>Página sobre Animes</title>
+</head>
+<body>
+    <header>
+        <h1>Explorando Mundos e Emoções Animadas</h1>
+    </header>
+    <main>
+        <section id="intro">
+            <h3>O Mundo dos Animes</h3>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Os animes são formas populares de entretenimento que transcendem fronteiras culturais e conquistam corações em todo o mundo. Com suas narrativas envolventes, personagens cativantes e estilos de animação únicos, os animes têm a capacidade de transportar os espectadores para mundos imaginários repletos de aventura, emoção e reflexão.
+            </p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Desde suas origens no Japão até sua influência global, os animes se transformaram em um fenômeno cultural que transcende fronteiras geográficas e gerações. O surgimento dos animes remonta ao início do século 20, com obras como "Astro Boy" que pavimentaram o caminho para o desenvolvimento desse estilo distintivo de animação. No entanto, foi nas décadas seguintes que os animes começaram a ganhar maior reconhecimento internacional. A popularidade de séries como "Dragon Ball", "Naruto" e "Sailor Moon" na década de 1990 ajudou a estabelecer uma base de fãs global e a despertar o interesse por essa forma de entretenimento. A internet e a disseminação de plataformas de streaming permitiram que pessoas ao redor do mundo tivessem acesso a uma vasta gama de animes, independentemente de sua localização geográfica.
+            </p>
+        </section>
+        <section id="anime-list">
+            <h2>Animes Populares</h2>
+            <table>
+                <tr>
+                    <td><img src="image/anime1.jpg" alt="Anime 1"></td>
+                    <td><img src="image/anime2.jpg" alt="Anime 2"></td>
+                    <td><img src="image/anime3.jpg" alt="Anime 3"></td>
+                </tr>
+            </table>
+        </section>
+        <section id="outro">
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Se você também é um entusiasta de animes, veio ao lugar certo! Nossa página é um santuário virtual dedicado a todos os apaixonados por essa forma única de entretenimento. Aqui, mergulharemos juntos em um oceano de narrativas envolventes, personagens memoráveis e estilos de animação que vão despertar sua imaginação. Explore nossa coleção cuidadosamente selecionada de resenhas, recomendações e análises aprofundadas das séries e filmes de anime mais impactantes. Quer você seja um novato curioso ou um veterano experiente, estamos aqui para compartilhar nossa paixão e conhecimento sobre o universo dos animes.
+            </p>
+        </section>
+    </main>
+    <footer>
+        <a href="php/etapa1.php">Compra de Produtos</a>
+    </footer>
+</body>
+</html>
